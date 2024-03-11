352639b8141f3b51f8283a3543c62aa7f00ddb81
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2024-03-11 13:28:11 +0100

    Slides l03_flem

--- my_scripts/02_least_squares_regression/02_least_squares_regression.R
+++ my_scripts/02_least_squares_regression/02_least_squares_regression.R
@@ -1,12 +1,33 @@
 #' ---
-#' title: Verify Least Squares Regression
+#' title: Validation of Least Squares Regression
 #' date: 2024-03-04 
 #' ---
 
-s_bw_bc_url_csv <- "https://charlotte-ngs.github.io/asmasss2024/data/asm_bw_bc_reg.csv"
-df_bw_bc_csv <- read.table(s_bw_bc_url_csv, header = TRUE, sep = ",")
-df_bw_bc_csv
+# read beef cattle data
+s_bw_bc_url <- "https://charlotte-ngs.github.io/asmasss2024/data/asm_bw_bc_reg.csv"
+df_bw_bc <- read.table(s_bw_bc_url, header = T, sep = ",")
+df_bw_bc
 
-N <- nrow(df_bw_bc_csv)
-x_dot_bar <- mean(df_bw_bc_csv[,"Breast.Circumference"])
+
+# Computations for slope estimate
+x <- df_bw_bc$Breast.Circumference
+y <- df_bw_bc$Body.Weight
+N <- nrow(df_bw_bc)
+
+x_y_dot <- sum(x*y)
+x_dot_bar <- mean(x)
+y_dot_bar <- mean(y)
+x_sq_dot <- sum(x*x)
+b_1_hat <- (x_y_dot - N * x_dot_bar * y_dot_bar) / (x_sq_dot - N * x_dot_bar^2)
+b_1_hat
+
+
+# Compuations for intercept
+b_0_hat <- y_dot_bar - b_1_hat * x_dot_bar
+b_0_hat
+
+
+# Validation
+lm_bw_bc <- lm(Body.Weight ~ Breast.Circumference, data = df_bw_bc)
+summary(lm_bw_bc)
 
